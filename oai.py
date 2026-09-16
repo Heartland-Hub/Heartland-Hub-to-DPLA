70008eba9c429f77a4322b9243efc21cf09093a4
Julia Bauder <julia.bauder@gmail.com>
2026-09-16 16:57:20 -0500

    Increase timeouts for SGCL

--- oai.py
+++ oai.py
@@ -247,7 +247,11 @@ class OAI:
                 timeouts += 1
                 print("\nRequest timed out. Waiting 5 seconds and trying again. Attempt {}".format(timeouts))
                 time.sleep(5)
-                if timeouts == 5:
+                # SGCL has close to 300,000 items and a tendency to time out every 40,000 records or so, so 5 timeouts wasn't enough
+                if self.id == 'sgcl' and timeouts == 10: 
+                    print("\nRequest has timed out 10 times. Stopping harvest.")
+                    break
+                elif timeouts == 5:
                     print("\nRequest has timed out 5 times. Stopping harvest.")
                     break
                 continue
